e14deac2c436d55b06457b56f5dac6914a6cad73
petrkubat <nando128@gmail.com>
2017-02-26 20:41:58 +0100

    Added basic SQLTest

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,7 @@
   * Created by petrkubat on 26/02/2017.
   */
 object Main {
-  val tests = List(GroupByTest)
+  val tests = List(new SQLTest(10000))
 
   def main(args: Array[String]): Unit = {
     AdaptiveTestRunner.runTests(tests, CaseGenerator)
---
+++ src/main/scala/SQLTest.scala
@@ -0,0 +1,39 @@
+import datatypes.Record
+
+import scala.util.Random
+
+/**
+  * Created by petrkubat on 26/02/2017.
+  */
+class SQLTest(val dataSize: Int) extends AdaptiveTest {
+  private def generateData(count: Int) : Seq[Record] = {
+    val rand = new Random(42)
+
+    val data = new Array[Record](count)
+    for (i <- 0 until count) {
+      import datatypes.RandomExtensions._
+      data(i) = rand.nextRecord
+    }
+
+    return data
+  }
+
+  override def runTest(configuration: AdaptiveConfiguration): Unit = {
+    val spark = initSpark(configuration)
+    val sqlContext = spark.sqlContext
+
+    import spark.implicits._
+
+    val data1 = sqlContext.createDataset(generateData(dataSize))
+    //val data2 = sqlContext.createDataset(generateData(dataSize))
+
+    val result = data1
+      .filter(_.recordType > 5)
+      .groupByKey(_.size)
+      .reduceGroups((left, right) => new Record(left.recordType, left.size, left.data, left.users + right.users))
+      .filter(_._2.users > 200)
+      .count()
+
+    println(result)
+  }
+}
